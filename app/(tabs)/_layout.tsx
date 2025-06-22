ec21504a34315451e00eb57883858c7216b75537
Aarón Portobanco <aaronportobanco@gmail.com>
2025-06-22 14:27:02 -0600

    Add layout and tab navigation with screens for Home, Search, Saved, and Profile; implement color scheme toggle and add movie details interfaces

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,45 @@
+import { View, Text } from "react-native";
+import { Home, Search, UserRound, Bookmark } from "lucide-react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarLabel: "Home",
+          tabBarIcon: (props) => <Home {...props} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarLabel: "Search",
+          tabBarIcon: (props) => <Search {...props} />,
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          tabBarLabel: "Saved",
+          tabBarIcon: (props) => <Bookmark {...props} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarLabel: "Profile",
+          tabBarIcon: (props) => <UserRound {...props} />,
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default Layout;

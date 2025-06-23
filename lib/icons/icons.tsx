86b80f12242ac6d3f0435dfdd59c6a3651a783a7
Aarón Portobanco <aaronportobanco@gmail.com>
2025-06-23 10:34:38 -0600

    Add iconWithClassName utility and export icon components for Sun, Moon, and Search

---
+++ lib/icons/icons.tsx
@@ -0,0 +1,7 @@
+// lib/icons.ts
+import { Sun, Moon, Search } from "lucide-react-native";
+import { iconWithClassName } from "./iconWithClassName";
+
+export const IconSun = iconWithClassName(Sun);
+export const IconMoon = iconWithClassName(Moon);
+export const IconSearch = iconWithClassName(Search);

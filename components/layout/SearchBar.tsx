5dc8dc2273a74a9b9634feb1be61c190800418a9
Aarón Portobanco <aaronportobanco@gmail.com>
2025-06-23 16:23:35 -0600

    Refactor SearchBar component to accept value and onChangeText props for better state management

--- components/layout/SearchBar.tsx
+++ components/layout/SearchBar.tsx
@@ -5,19 +5,27 @@ import { IconSearch } from "@/lib/icons/icons";
 interface Props {
   placeholder: string;
   onPress?: () => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: Props) => {
+const SearchBar = ({
+  placeholder,
+  onPress,
+  value,
+  onChangeText,
+}: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <IconSearch className="h-5 w-5 text-[#ab8bff]" />
       <TextInput
         onPress={onPress}
-        onChangeText={() => {}}
-        value=""
+        onChangeText={onChangeText}
+        value={value}
         placeholder={placeholder}
         placeholderTextColor="#a8b5db"
         className="flex-1 ml-2 text-white"
+        returnKeyType="search" // Muestra "search" en el teclado
       />
     </View>
   );

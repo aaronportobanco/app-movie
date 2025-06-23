411cf78f31fa7eb6bb3672c177864840ad016aeb
Aarón Portobanco <aaronportobanco@gmail.com>
2025-06-23 13:24:27 -0600

    Add play and star icons; update icons import in icons.tsx

--- lib/icons/icons.tsx
+++ lib/icons/icons.tsx
@@ -1,7 +1,8 @@
 // lib/icons.ts
-import { Sun, Moon, Search } from "lucide-react-native";
+import { Sun, Moon, Search, Star } from "lucide-react-native";
 import { iconWithClassName } from "./iconWithClassName";
 
 export const IconSun = iconWithClassName(Sun);
 export const IconMoon = iconWithClassName(Moon);
 export const IconSearch = iconWithClassName(Search);
+export const IconStar = iconWithClassName(Star);

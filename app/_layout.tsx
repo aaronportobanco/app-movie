35226e1f4440efb9770d9ce1bfdf614ee4fe8232
Aarón Portobanco <aaronportobanco@gmail.com>
2025-06-22 20:47:31 -0600

    Refactor layout component for improved readability and organization

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,70 +1,90 @@
-import './globals.css';
+import "./globals.css";
+import {
+  Theme,
+  ThemeProvider,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { Platform } from "react-native";
+import { NAV_THEME } from "../constants/constants";
+import { useColorScheme } from "../lib/useColorScheme";
 
-  import { Theme, ThemeProvider, DefaultTheme, DarkTheme } from '@react-navigation/native';
-  import { Stack } from 'expo-router';
-  import { StatusBar } from 'expo-status-bar';
-  import * as React from 'react';
-  import { Platform } from 'react-native';
-  import { NAV_THEME } from '../constants/constants';
-  import { useColorScheme } from '../lib/useColorScheme';
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
 
-  const LIGHT_THEME: Theme = {
-    ...DefaultTheme,
-    colors: NAV_THEME.light,
-  };
-  const DARK_THEME: Theme = {
-    ...DarkTheme,
-    colors: NAV_THEME.dark,
-  };
-
-  export {
+export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-  } from 'expo-router';
+} from "expo-router";
 
-  export default function RootLayout() {
-    const hasMounted = React.useRef(false);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { colorScheme, isDarkColorScheme } = useColorScheme();
-    const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
 
-    useIsomorphicLayoutEffect(() => {
-      if (hasMounted.current) {
-        return;
-      }
+  const hasMounted = React.useRef(false);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
-      if (Platform.OS === 'web') {
-        // Adds the background color to the html element to prevent white background on overscroll.
-        document.documentElement.classList.add('bg-background');
-      }
-      setIsColorSchemeLoaded(true);
-      hasMounted.current = true;
-    }, []);
-
-    if (!isColorSchemeLoaded) {
-      return null;
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
     }
 
-    return (
-      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-        <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
-        <Stack >
-          <Stack.Screen
-            name="(tabs)"
-            options={{
-              headerShown: false, // Hide the header for the tabs screen
-            }}
-          />
-          <Stack.Screen
-            name="movies/[id]"
-            options={{
-              headerShown: false, // Hide the header for the movie details screen
-            }}
-          />
-        </Stack>
-      </ThemeProvider>
-    );
+    if (Platform.OS === "web") {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add("bg-background");
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded || !fontsLoaded) {
+    return null;
   }
 
-  const useIsomorphicLayoutEffect =
-    Platform.OS === 'web' && typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;
+  return (
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false, // Hide the header for the tabs screen
+          }}
+        />
+        <Stack.Screen
+          name="movies/[id]"
+          options={{
+            headerShown: false, // Hide the header for the movie details screen
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
+  );
+}
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === "web" && typeof window === "undefined"
+    ? React.useEffect
+    : React.useLayoutEffect;
